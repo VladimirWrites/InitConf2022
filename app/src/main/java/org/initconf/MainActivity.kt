7e0c23696c3cd11412b1fc56c03ec60a6b01f9b2
VladimirWrites <write@vladimirj.dev>
2022-08-14 18:11:01 +0200

    3b - Add TalksList composable function

--- app/src/main/java/org/initconf/MainActivity.kt
+++ app/src/main/java/org/initconf/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -33,6 +35,15 @@ class MainActivity : ComponentActivity() {
                     TalkCard(talk)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TalksList(talks: List<Talk>) {
+    LazyColumn() {
+        items(talks) { talk ->
+            TalkCard(talk)
         }
     }
 }
