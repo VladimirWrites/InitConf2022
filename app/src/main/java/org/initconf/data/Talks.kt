d512fe95246a4c3af29c99deadf65bea52e41829
VladimirWrites <write@vladimirj.dev>
2022-08-14 17:25:51 +0200

    2b - Add hardcoded talk

---
+++ app/src/main/java/org/initconf/data/Talks.kt
@@ -0,0 +1,12 @@
+package org.initconf.data
+
+import org.initconf.model.Speaker
+import org.initconf.model.Talk
+
+val talk = Talk(
+    "Test Talk",
+    Speaker(
+        "Marko Marković",
+        "Android Developer"
+    )
+)
