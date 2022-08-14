e2a7e1e0d9bbe72051c2ddf83d27516b9a740ea8
VladimirWrites <write@vladimirj.dev>
2022-08-14 18:09:40 +0200

    3a - Add list of talks

--- app/src/main/java/org/initconf/data/Talks.kt
+++ app/src/main/java/org/initconf/data/Talks.kt
@@ -11,3 +11,134 @@ val talk = Talk(
         "https://initconf.org/assets/images/team/5zidoo.jpg"
     )
 )
+
+val talks = listOf(
+    Talk(
+        title = "HOW TO IMPLEMENT (_BUZZWORD_)",
+        speaker = Speaker(
+            name = "Timothy Mamo",
+            title = "Senior Cloud Native Engineer",
+            image = "https://initconf.org/assets/images/speakers/timothy.jpg"
+        )
+    ),
+    Talk(
+        title = "A GUIDE TO FUNCTIONAL PROGRAMMING",
+        speaker = Speaker(
+            name = "Jemima Abu",
+            title = "Front End Developer",
+            image = "https://initconf.org/assets/images/speakers/jemima.jpg"
+        )
+    ),
+    Talk(
+        title = "ESCAPING DEVELOPER NIGHTMARES",
+        speaker = Speaker(
+            name = "Rustam Mehmandarov",
+            title = "Passionate computer scientist",
+            image = "https://initconf.org/assets/images/speakers/rustam.jpg"
+        )
+    ),
+    Talk(
+        title = "REMIX YOUR UI & UX TO ANOTHER LEVEL",
+        speaker = Speaker(
+            name = "Arisa Fukuzaki",
+            title = "Developer Relations Engineer at Storyblok",
+            image = "https://initconf.org/assets/images/speakers/arisa.jpg"
+        )
+    ),
+    Talk(
+        title = "STOP USING JAVASCRIPT FOR THAT: MOVING FEATURES FROM JS TO CSS AND HTML",
+        speaker = Speaker(
+            name = "Kilian Valkhof",
+            title = "Founder at Polypane browser for developers",
+            image = "https://initconf.org/assets/images/speakers/kilian.jpg"
+        )
+    ),
+    Talk(
+        title = "WILL ARTIFICIAL INTELLIGENCE (AI) REPLACE OR ASSIST DEVELOPERS?",
+        speaker = Speaker(
+            name = "Tomislav Bronzin",
+            title = "Digital Transformation Advisor",
+            image = "https://initconf.org/assets/images/speakers/tomislav.jpg"
+        )
+    ),
+    Talk(
+        title = "ORDERING THE CHAOS - CLEANING LOGS AND ORDERING EVENTS IN MICROSERVICES",
+        speaker = Speaker(
+            name = "Adam Furmanek",
+            title = "Software Developer, Blogger, Public Speaker, Book Writer",
+            image = "https://initconf.org/assets/images/speakers/adam.jpg"
+        )
+    ),
+    Talk(
+        title = "DARK SECRETS OF THE EVENT LOOP",
+        speaker = Speaker(
+            name = "Vekoslav Stefanovski",
+            title = "Head of Development at Sourcico",
+            image = "https://initconf.org/assets/images/speakers/vekoslav.jpg"
+        )
+    ),
+    Talk(
+        title = "CLEAN SOFTWARE ARCHITECTURE WITH CLEAN CODE TIPS",
+        speaker = Speaker(
+            name = "Boris Janjanin",
+            title = "ProductDock, Senior software engineer",
+            image = "https://initconf.org/assets/images/speakers/boris.jpg"
+        )
+    ),
+    Talk(
+        title = "SO, YOU WANNA BE A GAME DEVELOPER?",
+        speaker = Speaker(
+            name = "Leo Tot",
+            title = "Your Game Dev Senpai",
+            image = "https://initconf.org/assets/images/speakers/leo.jpg"
+        )
+    ),
+    Talk(
+        title = "FROM THE SIX FEET UNDER, TO THE STARS AND ABOVE",
+        speaker = Speaker(
+            name = "Romeo Mlinar",
+            title = "Never Give Up!",
+            image = "https://initconf.org/assets/images/speakers/romeo.jpg"
+        )
+    ),
+    Talk(
+        title = "AZURE VS AWS, CONTAINERS, SCALING, K8S PLATFORMS, COST, WINNER",
+        speaker = Speaker(
+            name = "Ratomir Vukadin",
+            title = "Devtech group, Software Engineer",
+            image = "https://initconf.org/assets/images/speakers/ratomir.jpg"
+        )
+    ),
+    Talk(
+        title = "FROM THE SIX FEET UNDER, TO THE STARS AND ABOVE",
+        speaker = Speaker(
+            name = "Nenad Trajkovski",
+            title = "Deda",
+            image = "https://initconf.org/assets/images/speakers/deda.jpg"
+        )
+    ),
+    Talk(
+        title = "THERE AND BACK AGAIN OF SOFTWARE ARCHITECTURE: MONOLITHS VS. MICROSERVICES",
+        speaker = Speaker(
+            name = "Zoran Horvat",
+            title = "Principal consultant at Coding Helmet s.p.r.l.",
+            image = "https://initconf.org/assets/images/speakers/zoran.jpg"
+        )
+    ),
+    Talk(
+        title = "WILL ARTIFICIAL INTELLIGENCE (AI) REPLACE OR ASSIST DEVELOPERS?",
+        speaker = Speaker(
+            name = "Arian Stipić",
+            title = "Digital Architect",
+            image = "https://initconf.org/assets/images/speakers/arian.jpg"
+        )
+    ),
+    Talk(
+        title = "SOLUTION ARCHITECTURE - STRATEGIC BETS IN THE FACE OF UNCERTAINTY",
+        speaker = Speaker(
+            name = "Aleksandar Kahriman",
+            title = "Tech Excellence Office of HTEC Group",
+            image = "https://initconf.org/assets/images/speakers/aleksandar.jpeg"
+        )
+    )
+)
