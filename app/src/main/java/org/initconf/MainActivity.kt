85897c93453b8e50cdbc19f4e036fb8db9e9b468
VladimirWrites <write@vladimirj.dev>
2022-08-14 17:50:30 +0200

    2j - Make image round

--- app/src/main/java/org/initconf/MainActivity.kt
+++ app/src/main/java/org/initconf/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import org.initconf.data.talk
 import org.initconf.model.Talk
@@ -36,7 +41,11 @@ fun TalkCard(talk: Talk) {
     Row() {
         AsyncImage(
             model = talk.speaker.image,
-            contentDescription = "Image of ${talk.speaker}"
+            contentDescription = "Image of ${talk.speaker}",
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(60.dp)
         )
         Column() {
             Text(text = talk.title)
