7d6dbfd1a072ea8279288813035214655f5fab5c
VladimirWrites <write@vladimirj.dev>
2022-08-14 17:33:21 +0200

    2e - Organize fields inside Column

--- app/src/main/java/org/initconf/MainActivity.kt
+++ app/src/main/java/org/initconf/MainActivity.kt
@@ -3,6 +3,7 @@ package org.initconf
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -30,9 +31,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TalkCard(talk: Talk) {
-    Text(text = talk.title)
-    Text(text = talk.speaker.name)
-    Text(text = talk.speaker.title)
+    Column() {
+        Text(text = talk.title)
+        Text(text = talk.speaker.name)
+        Text(text = talk.speaker.title)
+    }
 }
 
 @Preview(showBackground = true)
